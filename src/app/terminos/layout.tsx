a17af8b24c82f6ef20272f2036230a92d62d26e3
g3deon <123526476+katia-tsx@users.noreply.github.com>
2024-03-17 09:07:20 -0400

    terminos y condiciones✅  politica de privacidad✅

---
+++ src/app/terminos/layout.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+export default function AboutLayout({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<Container className="max-w-5xl pt-32 pb-12">
+			<article className="mx-auto space-y-8">{children}</article>
+		</Container>
+	);
+}
